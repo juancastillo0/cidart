cd0af17b9e289907e10cccfa70bfdd01235ab7d9
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-07-28 05:40:11 +0000

    MobX writePolicy: ReactiveWritePolicy.never

--- web_client/lib/main.dart
+++ web_client/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:jaspr/jaspr.dart';
+import 'package:mobx/mobx.dart';
 import './app.dart';
 
 void main() {
+  mainContext.config = mainContext.config.clone(
+    writePolicy: ReactiveWritePolicy.never,
+  );
   runApp(App());
 }
