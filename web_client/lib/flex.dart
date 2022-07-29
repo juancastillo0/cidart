ccb47b6406a31e23b78b77c98944111aa4460ff6
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-07-29 03:57:31 +0000

    Col, Row, Space and styling

--- web_client/lib/flex.dart
+++ web_client/lib/flex.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:web_client/prelude.dart';
+
 enum AxisAlign {
   start,
   end,
@@ -19,26 +23,100 @@ enum FlexWrap {
   nowrap,
 }
 
+enum FlexDirection {
+  row,
+  column,
+}
+
 extension FlexWrapExt on FlexWrap {
   String get nameHtml => toString().split('.').last.replaceAll('_', '-');
 }
 
 String flexStyle({
-  required bool column,
+  required FlexDirection direction,
   AxisAlign main = AxisAlign.start,
   AxisAlign cross = AxisAlign.center,
   FlexWrap? wrap,
 }) {
-  return 'display:flex;${column ? 'flex-direction:column;' : ''}'
+  return 'display:flex;flex-direction:${direction.name};'
       'justify-content:${main.nameHtml};align-items:${cross.nameHtml};'
       '${wrap == null ? '' : 'flex-wrap:${wrap.nameHtml};'}';
   // '${expand ? column ? 'height:100%;' : 'width:100%;' : ''}' ${column ? 'width:100%;' : 'height:100%;'}
 }
 
-String flexCenter({bool column = false}) {
+String flexCenter(FlexDirection direction) {
   return flexStyle(
     main: AxisAlign.center,
     cross: AxisAlign.center,
-    column: column,
+    direction: direction,
   );
 }
+
+class Col extends StatelessObserverComponent {
+  const Col(
+    this.children, {
+    super.key,
+    this.main = AxisAlign.start,
+    this.cross = AxisAlign.center,
+    this.wrap,
+    this.styles,
+  });
+
+  final List<Component> children;
+  final AxisAlign main;
+  final AxisAlign cross;
+  final FlexWrap? wrap;
+  final Styles? styles;
+
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield div(
+      attributes: {
+        'style': flexStyle(
+              direction: FlexDirection.column,
+              cross: cross,
+              main: main,
+              wrap: wrap,
+            ) +
+            _stylesToCssString(styles)
+      },
+      children,
+    );
+  }
+}
+
+class Row extends StatelessObserverComponent {
+  const Row(
+    this.children, {
+    super.key,
+    this.main = AxisAlign.start,
+    this.cross = AxisAlign.center,
+    this.wrap,
+    this.styles,
+  });
+
+  final List<Component> children;
+  final AxisAlign main;
+  final AxisAlign cross;
+  final FlexWrap? wrap;
+  final Styles? styles;
+
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield div(
+      attributes: {
+        'style': flexStyle(
+              direction: FlexDirection.row,
+              cross: cross,
+              main: main,
+              wrap: wrap,
+            ) +
+            _stylesToCssString(styles)
+      },
+      children,
+    );
+  }
+}
+
+String _stylesToCssString(Styles? styles) =>
+    styles?.styles.entries.map((e) => '${e.key}:${e.value};').join() ?? '';
---
+++ web_client/lib/spacing.dart
@@ -0,0 +1,9 @@
+import 'prelude.dart';
+
+class Space {
+  const Space._();
+  static final hSmall = span(styles: Styles.box(width: Unit.pixels(10)), []);
+  static final vSmall = span(styles: Styles.box(height: Unit.pixels(10)), []);
+  static final hMid = span(styles: Styles.box(width: Unit.pixels(20)), []);
+  static final vMid = span(styles: Styles.box(height: Unit.pixels(20)), []);
+}
